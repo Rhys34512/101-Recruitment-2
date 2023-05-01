1cf1bc1939d0aff8f1fbe9271c0d1b56658ca730
Coritux <41597371+coritux@users.noreply.github.com>
2023-05-01 16:06:56 +1000

    Updated Homepage
    Incorporated Global Footer specified in the "footer.inc" PHP file.

--- index.php
+++ index.php
@@ -134,49 +134,9 @@
 <!-- this the footer which will be at the bottom of the page which is broken into 4 divs, main including company logo,
 links for the different pages within the website,
 location of our headquaters and our socials -->
-	    <footer>
-	        <div id="footer-main">
-	            <img src="images/logodarkmode.png" title="Company Logo" width="115" height="65" id="footer-logo">
-	            <p class="footer-sub">Proudly delivering unique and industry-leading solutions to companies around the world.</p>
-	        </div>
-	        <div id="footer-links">
-	                <p class="footer-section-title">Links</p>
-	                <ul id="footer-nav">
-	                    <li>
-	                        <a href="index.html" class="footer-nav-link">Home</a>
-	                    </li>
-	                    <li>
-	                        <a href="jobs.html" class="footer-nav-link">Job Openings</a>
-	                    </li>
-	                    <li>
-	                        <a href="apply.html" class="footer-nav-link">Submit Application</a>
-	                    </li>
-	                    <li>
-	                        <a href="services.html" class="footer-nav-link">Services</a>
-	                    </li>
-	                    <li>
-	                        <a href="enhancements.html" class="footer-nav-link">Enhancements</a>
-	                    </li>
-	                    <li>
-	                        <a href="about.html" class="footer-nav-link">About Us</a>
-	                    </li>
-	                 </ul>
-	        </div>
-	        <div id="footer-location">
-	            <p class="footer-section-title">Our Location</p>
-	            <p class="footer-location-body">Swing by our office:</p>
-	            <p class="footer-location-body">427-451 Burwood Rd, Hawthorn, VIC 3122, Australia</p>
-	        </div>
-	        <div id="footer-socials">
-	            <p class="footer-section-title">Check out our Socials</p>
-	            <div id="social-icon-group">
-	                <a href="https://facebook.com/101-Recruitment"><img src="images/facebook.png" class="social-icon" title="Facebook"></a>
-	                <a href="https://twitter.com/101-Recruitment"><img src="images/twitter.png" class="social-icon" title="Twitter"></a>
-	                <a href="https://instagram.com/101-Recruitment"><img src="images/instagram.png" class="social-icon" title="Instagram"></a>
-	                <a href="https://youtube.com/101-Recruitment"><img src="images/youtube.png" class="social-icon" title="YouTube"></a>
-	            </div>
-	        </div>
-	        <p id="footer-sub-copy">Copyright &copy; 2023 101 IT. All rights reserved.</p>
-	    </footer>
+	    
+		<?php include_once("footer.inc"); ?>
+		
+		
 	</body>
 	</html>
