d44125f4ec40350e5ea4584899f4fc775b755b41
EddyRedd <127158501+EddyRedd@users.noreply.github.com>
2023-05-01 14:43:38 +1000

    Update manage.php

--- manage.php
+++ manage.php
@@ -1,2 +1,16 @@
 <html>
- <?html>
+ <head>
+<!-- 
+ Create a web page manage.php that allows a manager to make the following queries of
+ the eoi table and returns a web page with the appropriate results.
+  • List all EOIs.
+  • List all EOIs for a particular position (given a job reference number).
+  • List all EOIs for a particular applicant given their first name, last name or both.
+  • Delete all EOIs with a specified job reference number
+  • Change the Status of an EOI.
+ -->
+ </head>
+ <body>
+ 
+ </body>
+</html>
