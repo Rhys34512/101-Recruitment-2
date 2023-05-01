a97136146dea9fcec159526efbe9165ca43848e7
Coritux <41597371+coritux@users.noreply.github.com>
2023-05-01 15:31:12 +1000

    Updated Settings File
    Updated Database Configuration file to incorporate the credentials of the database, allowing for other pages in the website to connect to them.

--- settings.php
+++ settings.php
@@ -1,1 +1,14 @@
+<?php
 
+$serverName = "feenix-mariadb-web.swin.edu.au";
+$dBUsername = "s104558257";
+$dBPassword = "Sw1n.608#";
+$dBName = "s104558257_db";
+
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+if (!conn) {
+    die("Connection failed: " . mysqli_connect_error()); 
+}
+
+?>
