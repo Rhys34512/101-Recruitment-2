c365edb984b52ad155e425fd885fdfa31fb832e8
Coritux <41597371+coritux@users.noreply.github.com>
2023-05-01 16:21:10 +1000

    Updated Enhancements Page
    Incorporated Global Navigation Menu and Footer specified in the "menu.inc" and "footer.inc" PHP files.

--- enhancements.php
+++ enhancements.php
@@ -11,33 +11,9 @@
       </head>
 <body>
 <!-- Container which holds the navigation bar -->
-     <div id="container">
-        <nav>
-            <div class="navigation">
-                <a href="index.html" title="Return to Homepage"><img src="images/logo.png" class="logo" alt="101 IT Company Logo"></a>
-                <ul class="navigation">
-                    <li class="nav">
-                        <a href="index.html" class="link">Home</a>
-                    </li>
-                    <li class="nav">
-                        <a href="jobs.html" class="link">Job Openings</a>
-                    </li>
-                    <li class="nav">
-                        <a href="apply.html" class="link">Submit Application</a>
-                    </li>
-                    <li class="nav">
-                        <a href="services.html" class="link">Services</a>
-                    </li>
-                    <li class="nav">
-                        <a href="enhancements.html" class="link">Enhancements</a>
-                    </li>
-                    <li class="nav">
-                        <a href="about.html" class="link">About Us</a>
-                    </li>
-                 </ul>
-            </div>
-        </nav>
-    </div>
+     
+	<?php include_once("menu.inc"); ?>
+	
 	<div class="sec">
 	<!-- div being used to seperate the content within it using the class "sec" which is used for various pages to be styled consistently throughout the website -->
    <h2 id="enhancements-header">Enhancements</h2> <!-- Header of the page having an id so it can be styled in CSS-->
@@ -63,49 +39,8 @@
 		a physical address, 
 		and a set of social media links.
  -->
-	<footer>
-        <div id="footer-main">
-            <img src="images/logodarkmode.png" title="Company Logo" width="115" height="65" id="footer-logo">
-            <p class="footer-sub">Proudly delivering unique and industry-leading solutions to companies around the world.</p>
-        </div>
-        <div id="footer-links">
-                <p class="footer-section-title">Links</p>
-                <ul id="footer-nav">
-                    <li>
-                        <a href="index.html" class="footer-nav-link">Home</a>
-                    </li>
-                    <li>
-                        <a href="jobs.html" class="footer-nav-link">Job Openings</a>
-                    </li>
-                    <li>
-                        <a href="apply.html" class="footer-nav-link">Submit Application</a>
-                    </li>
-                    <li>
-                        <a href="services.html" class="footer-nav-link">Services</a>
-                    </li>
-                    <li>
-                        <a href="enhancements.html" class="footer-nav-link">Enhancements</a>
-                    </li>
-                    <li>
-                        <a href="about.html" class="footer-nav-link">About Us</a>
-                    </li>
-                 </ul>
-        </div>
-        <div id="footer-location">
-            <p class="footer-section-title">Our Location</p>
-            <p class="footer-location-body">Swing by our office:</p>
-            <p class="footer-location-body">427-451 Burwood Rd, Hawthorn, VIC 3122, Australia</p>
-        </div>
-        <div id="footer-socials">
-            <p class="footer-section-title">Check out our Socials</p>
-            <div id="social-icon-group">
-                <a href="https://facebook.com/101-Recruitment"><img src="images/facebook.png" class="social-icon" title="Facebook"></a>
-                <a href="https://twitter.com/101-Recruitment"><img src="images/twitter.png" class="social-icon" title="Twitter"></a>
-                <a href="https://instagram.com/101-Recruitment"><img src="images/instagram.png" class="social-icon" title="Instagram"></a>
-                <a href="https://youtube.com/101-Recruitment"><img src="images/youtube.png" class="social-icon" title="YouTube"></a>
-            </div>
-        </div>
-        <p id="footer-sub-copy">Copyright &copy; 2023 101 IT. All rights reserved.</p>
-    </footer>
+	
+	<?php include_once("footer.inc"); ?>
+	
 </body>
 </html>
