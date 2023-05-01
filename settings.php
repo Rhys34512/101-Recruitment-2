ff64e1b4254b39cc10ed178bdfe830fd31d07fda
Coritux <41597371+coritux@users.noreply.github.com>
2023-05-01 16:10:12 +1000

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$serverName = "feenix-mariadb-web.swin.edu.au";
+$serverName = "feenix-mariadb.swin.edu.au";
 $dBUsername = "s104558257";
 $dBPassword = "Sw1n.608#";
 $dBName = "s104558257_db";
