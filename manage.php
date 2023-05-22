caebf0c2670a8069e014caca37bafcb39676b5c5
EddyRedd <127158501+EddyRedd@users.noreply.github.com>
2023-05-22 11:15:13 +1000

    Update manage.php

--- manage.php
+++ manage.php
@@ -1,5 +1,5 @@
 <html>
-    <head>
+ <head>
  <link rel="stylesheet" href="styles/style.css">
  </head>
  <body>
@@ -8,169 +8,187 @@
     
 	if($conn)
     {
+		$target_jobNum = "All";
+		
+		if(isset($_POST["jobNumber"]))
+		{
+			$target_jobNum = $_POST["jobNumber"];
+		}
+
         $query = "SELECT * FROM eoi";
         $result = mysqli_query($conn, $query);
-        if($result)
-        {
-            $record = mysqli_fetch_assoc($result);
-            if($record)
-            {
-				echo "<p>At least 1 record retrieved</p>";
-				
-				$display = new Result();
-				$display->set_details_job($record["idEOI"], $record['numJob'], $record['txtStatus']);
-				
-				$txtStatus= $record['txtStatus'];
-				$idEOI = $record["idEOI"];
-				$numJOB = $record['numJob'];
-				
-				$txtFname = $record['txtFname'];
-				$txtLname = $record['txtLname']; 	
-				$txtBirthDate = $record['txtBirthDate']; 
-				$txtGender = $record['txtGender']; 
-				
-				$txtAddress = $record['txtAddress']; 
-				$txtState = $record['txtState']; 
-				$numPostcode = $record['numPostcode']; 
-				$txtEmail = $record['txtEmail'];
-				$txtPhone = $record['txtPhone']; 
-				
-				$txtSkills = $record['lstSkills']; 
-				$txtOtherSkills = $record['txtOtherSkills']; 
-				
+     
+		$rows = mysqli_num_rows($result);
+		if($result)
+			{
+			$append = ($rows > 1) ? "s": "";
+			echo "<p>$rows record$append retrieved for $target_jobNum</p>";
 		
-				$display->set_details_applicant($txtFname, $txtLname, $txtBirthDate, $txtGender);
-				$display->set_details_contact($txtAddress, $txtState, $numPostcode, $txtEmail, $txtPhone);
-				$display->set_details_skills ($txtSkills, $txtOtherSkills);
+			echo "<div class=\"wrapper\">";
+			echo "<div class=\"jobDescription\">";
+			
+			$jobIDs=array();
+			
+			for ($i = 0; $i < $rows; $i++)
+			{
+				$record = mysqli_fetch_assoc($result);
+			
+				if($record)
+				{
+					$txtStatus = $record['txtStatus'];
+					$idEOI = $record["idEOI"];
+					$numJOB = $record['numJob'];
 				
-				$display->display_applicant();				
-            }
-            else
-            {
-                echo "<p>No records retrieved</p>";
-            }
-        }
-        else
+					$txtFname = $record['txtFname'];
+					$txtLname = $record['txtLname']; 	
+					$txtBirthDate = $record['txtBirthDate']; 
+					$txtGender = $record['txtGender']; 
+				
+					$txtAddress = $record['txtAddress']; 
+					$txtState = $record['txtState']; 
+					$numPostcode = $record['numPostcode']; 
+					$txtEmail = $record['txtEmail'];
+					$txtPhone = $record['txtPhone']; 
+				
+					$txtSkills = $record['lstSkills']; 
+					$txtOtherSkills = $record['txtOtherSkills']; 
+					
+					if($target_jobNum == "All" || $numJOB == $target_jobNum)
+					{
+					$jobExists = false;
+					for ($i = 0; $i < count($jobIDs); $i++)
+					{
+						if($jobIDs[$i] == $numJOB)
+						{
+							$jobExists = true;
+						}
+					}
+					
+					if($jobExists == false)
+					{
+						array_push($jobIDs, $numJOB);
+					}
+					
+					echo "<section class=\"jobInfo\">";
+					echo "<h3>$numJOB - Applicant Details for: $txtFname $txtLname</h3>";
+					echo "<table border=\"0\" align=\"left\" width=\"1000px\">\n";
+		
+					echo "<tr>\n"
+						."<td> Expression of Interest #</td>"
+						."<td> $idEOI </td>"
+						."<td> Job Number #</td>"
+						."<td> $numJOB </td>"
+						."</tr>\n";
+		
+					echo "<tr>\n"
+						."<td> First Name:</td>"
+						."<td> $txtFname </td>"
+						."<td> Last Name:</td>"
+						."<td> $txtLname </td>"
+						."</tr>\n";
+		
+					echo "<tr>\n"
+						."<td> Birth date::</td>"
+						."<td> $txtBirthDate </td>"
+						."<td> Gender:</td>"
+						."<td> $txtGender </td>"
+						."</tr>\n";
+		
+					echo "<tr>\n"
+						."<td> </td>"
+						."<td> </td>"
+						."<td> </td>"
+						."<td> </td>"
+						."</tr>\n";
+
+					echo "<tr>\n"
+						."<td> Address:</td>"
+						."<td colspan=\"3\"> $txtAddress, $txtState $numPostcode</td>"
+						."</tr>\n";
+			
+					echo "<tr>\n"
+						."<td> </td>"
+						."<td> </td>"
+						."<td> </td>"
+						."<td> </td>"
+						."</tr>\n";
+			
+					echo "<tr>\n"
+						."<td> Contact Phone:</td>"
+						."<td> $txtPhone </td>"
+						."<td> E-mail:</td>"
+						."<td> $txtEmail </td>"
+						."</tr>\n";	
+			
+					echo "<tr>\n"
+						."<td> </td>"
+						."<td> </td>"
+						."<td> </td>"
+						."<td> </td>"
+						."</tr>\n";
+					echo "</table>";
+
+					echo "<h3>Relevant Skills</h3>";
+					echo "<p>$txtSkills</p>";
+				
+					echo "<h3>Other Skills</h3>";
+					echo "<p>$txtOtherSkills</p>";						
+
+					echo "</section>";
+				
+					echo"<hr>";
+					}
+				}
+			}
+			echo "</div>";
+			
+			echo "<aside class=\"jobSidebar\">";
+				echo "<form method=\"post\" action=\"manage.php\">";
+					echo "<fieldset>";
+					echo "<legend>Job Opening Search</legend>";
+					echo "<hr id=\"jobs-hr\">";
+				
+					echo "<label for=\"jobNumber\">";
+					echo "<select name=\"jobNumber\" id=\"jobNumber\">";
+					echo "<option value=\"All\">". All ."<br>";
+					for ($i = 0; $i < count($jobIDs); $i++)
+					{
+						echo "<option value=\"$jobIDs[$i]\">". $jobIDs[$i] ."<br>";	
+						echo "</option>";
+					}
+					echo "</select>";
+					
+					echo "<input type=\"submit\" id=\"submit\" value=\"Update Display\">";
+					echo "<br>";
+					echo "<input type=\"submit\" id=\"submit\" value=\"Delete Displayed Jobs\">";
+					echo "<br>";
+					echo "<input type=\"submit\" id=\"submit\" value=\"Display Specific Applicant\">";
+				
+					echo "</form>";
+				echo "</fieldset>";
+			echo "</aside>";
+			
+		}
+		else
         {
-            echo "<p>MySQL operation unsuccessful</p>"; 
-        }
+			echo "<p>No records retrieved</p>";
+		}
+    }
+    else
+    {
+		echo "<p>MySQL operation unsuccessful</p>"; 
 	}
      mysqli_close($conn);
-  ?>
-  
-  
-  <?php
-   class Result
-   {
-    public $idEOI;
-    public $numJOB;
-    public $txtFname;
-    public $txtLname;
-    public $txtBirthDate;
-    public $txtGender;
-    public $txtAddress;
-    public $txtState;
-    public $numPostcode;
-    public $txtEmail;
-    public $txtPhone;
-    public $txtSkills;
-    public $txtOtherSkills;
-    public $txtStatus;
-    
-    function set_details_job($idEOI, $numJOB, $txtStatus)
-    {
-     $this->idEOI = $idEOI;
-     $this->numJOB = $numJOB;
-    }
-    
-    function set_details_applicant($txtFname, $txtLname, $txtBirthDate, $txtGender)
-    {
-      $this->txtFname = $txtFname;
-      $this->txtLname = $txtLname;
-      $this->txtBirthDate = $txtBirthDate;
-      $this->txtGender = $txtGender;
-    }
-    
-    function set_details_contact($txtAddress, $txtState, $numPostcode, $txtEmail, $txtPhone)
-    {
-      $this->txtAddress = $txtAddress;
-      $this->txtState = $txtState;
-      $this->numPostcode = $numPostcode;
-      $this->txtEmail = $txtEmail;
-      $this->txtPhone = $txtPhone;
-    }
-    
-    function set_details_skills ($txtSkills, $txtOtherSkills)
-    {
-      $this->txtSkills = $txtSkills;
-      $this->txtOtherSkills = $txtOtherSkills;
-    }
-    
-    function display_applicant()
-    {
-		echo "<table border=\"0\">\n";
-		
-		echo "<tr>\n"
-			."<td> Expression of Interest #</td>"
-			."<td> $this->idEOI </td>"
-			."<td> Job Number #</td>"
-			."<td> $this->numJOB </td>"
-			."</tr>\n";
-		
-		echo "<tr>\n"
-			."<td> First Name:</td>"
-			."<td> $this->txtFname </td>"
-			."<td> Last Name:</td>"
-			."<td> $this->txtLname </td>"
-			."</tr>\n";
-		
-		echo "<tr>\n"
-			."<td> Birth date::</td>"
-			."<td> $this->txtBirthDate </td>"
-			."<td> Gender:</td>"
-			."<td> $this->txtGender </td>"
-			."</tr>\n";
-		
-		echo "<tr>\n"
-			."<td> </td>"
-			."<td> </td>"
-			."<td> </td>"
-			."<td> </td>"
-			."</tr>\n";
-
-		echo "<tr>\n"
-			."<td> Address:</td>"
-			."<td> $this->txtAddress, $this->txtState $this->numPostcode</td>"
-			."</tr>\n";
-			
-		echo "<tr>\n"
-			."<td> </td>"
-			."<td> </td>"
-			."<td> </td>"
-			."<td> </td>"
-			."</tr>\n";
-			
-		echo "<tr>\n"
-			."<td> Contact Phone:</td>"
-			."<td> $this->txtPhone </td>"
-			."<td> E-mail:</td>"
-			."<td> $this->txtEmail </td>"
-			."</tr>\n";	
-			
-		echo "</table>";
-	}
-   }
-  ?>
- 
+  ?> 
  </body>
 </html>
 
 <!-- 
  Create a web page manage.php that allows a manager to make the following queries of
  the eoi table and returns a web page with the appropriate results.
-  • List all EOIs.
-  • List all EOIs for a particular position (given a job reference number).
-  • List all EOIs for a particular applicant given their first name, last name or both.
+  • List all EOIs. done
+  • List all EOIs for a particular position (given a job reference number). done
   • Delete all EOIs with a specified job reference number
+  • List all EOIs for a particular applicant given their first name, last name or both.
   • Change the Status of an EOI.
  -->
