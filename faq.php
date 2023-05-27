72bb6c635c2bbdce638f72558e8c50e8fbefb6ec
Coritux <41597371+coritux@users.noreply.github.com>
2023-05-27 18:12:14 +1000

    Created FAQ Page
    Uploaded contents of the Frequently Asked Questions (FAQ) page, an enhancement providing guidance and clarity to users who may have queries regarding the website.

---
+++ faq.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Insert Description Here">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="author" content="Ruhul">
+
+    <title>Frequently asked questions?</title>
+    <link rel="stylesheet" href="faq_3.css">
+
+</head>
+<body>
+
+
+
+
+<div class="faq-main">
+  <div class="faq-container">
+    <h1>Frequently asked questions?</h1>
+
+    <div class="tab">
+      <input type="radio" name="acc" id="acc1">
+      <label for="acc1">
+        <h2>01</h2>
+        <h3>What are the values of 101 IT?</h3>
+      </label>
+      <div class="content">
+        <p>
+          101 IT is dedicated to providing quality services to clients and customers while reducing the stress and anxiety associated with finding a job.
+          We strive to exceed our clients' expectations and contribute to global issues through our sustainability initiative.
+        </p>
+      </div>
+    </div>
+
+    <div class="tab">
+      <input type="radio" name="acc" id="acc2">
+      <label for="acc2">
+        <h2>02</h2>
+        <h3>Are the job postings on 101 Recruitment reliable?</h3>
+      </label>
+      <div class="content">
+        <p>
+          Yes, we take pride in providing reliable job postings.
+          We ensure that all job listings on our platform are verified and legitimate, saving you from wasting time on fake or misleading job opportunities.
+        </p>
+      </div>
+    </div>
+
+    <div class="tab">
+      <input type="radio" name="acc" id="acc3">
+      <label for="acc3">
+        <h2>03</h2>
+        <h3>How can I search for job openings on 101 Recruitment?</h3>
+      </label>
+      <div class="content">
+        <p>
+          To search for job openings on 101 Recruitment, visit our Job Openings page on our website.
+          You can browse through the available listings and use filters to narrow down your search based on location, job category, and specific criteria.
+        </p>
+      </div>
+    </div>
+
+    <div class="tab">
+      <input type="radio" name="acc" id="acc4">
+      <label for="acc4">
+        <h2>04</h2>
+        <h3>How can I apply for a position at 101 IT?</h3>
+      </label>
+      <div class="content">
+        <p>
+          To apply for a position at 101 IT, please visit our Job Openings page and select the desired job listing.
+          Follow the instructions provided to submit your application.
+          We encourage you to apply for positions that align with your skills and interests.
+        </p>
+      </div>
+    </div>
+
+    <div class="tab">
+      <input type="radio" name="acc" id="acc5">
+      <label for="acc5">
+        <h2>05</h2>
+        <h3>How many job postings does 101 Recruitment have?</h3>
+      </label>
+      <div class="content">
+        <p>
+          We have over 150,000 jobs posted every month, offering a wide range of opportunities in various technology fields.
+          Our extensive network of partner companies allows us to provide a large number of job openings to our users.
+        </p>
+      </div>
+    </div>
+  </div>
+  </div>
+</body>
+</html> 
